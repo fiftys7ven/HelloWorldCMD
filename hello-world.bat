5549c5a60cc08a15e389b64adbb73de8d254a88b
Spilleparadis <playerwictor@gmail.com>
2023-10-17 19:26:09 +0200

    Create hello-world.bat

---
+++ hello-world.bat
@@ -0,0 +1,63 @@
+@echo off
+
+title Hello World!
+color 2f
+:a
+cls
+timeout /t 1 /nobreak >nul
+echo H
+echo H
+echo H
+timeout /t 1 /nobreak >nul
+cls
+echo He
+echo He
+echo He
+timeout /t 1 /nobreak >nul
+cls
+echo Hel
+echo Hel
+echo Hel
+timeout /t 1 /nobreak >nul
+cls
+echo Hell
+echo Hell
+echo Hell
+timeout /t 1 /nobreak >nul
+cls
+echo Hello 
+echo Hello 
+echo Hello 
+timeout /t 1 /nobreak >nul
+cls
+echo Hello W
+echo Hello W
+echo Hello W
+timeout /t 1 /nobreak >nul
+cls
+echo Hello Wo
+echo Hello Wo
+echo Hello Wo
+timeout /t 1 /nobreak >nul
+cls
+echo Hello Wor
+echo Hello Wor
+echo Hello Wor
+timeout /t 1 /nobreak >nul
+cls
+echo Hello Worl
+echo Hello Worl
+echo Hello Worl
+timeout /t 1 /nobreak >nul
+cls
+echo Hello World
+echo Hello World
+echo Hello World
+timeout /t 1 /nobreak >nul
+cls
+echo Hello World!
+echo Hello World!
+echo Hello World!
+timeout /t 1 /nobreak >nul
+cls
+goto a
